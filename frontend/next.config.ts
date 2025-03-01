41c9ab44c30d4a77318c84d1a9de85b9c7ce80a4
harmya <hvbhatt@purdue.edu>
2025-03-01 04:26:54 -0500

    bruh

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/thousand-words" : "",
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/thousand-words/" : "",
 };
 
 export default nextConfig;
